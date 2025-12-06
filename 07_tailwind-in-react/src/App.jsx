d9e1a90121cb148d14da26f18d53b2b27126c0dc
ADITYA SINGH <adityasinghk099@gmail.com>
2025-12-06 07:50:07 +0530

    learning Tailwind in React

---
+++ 07_tailwind-in-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+  return (
+    <>
+      <Card personName = "Rana" work = "web Developer" btnTxt = "Call"></Card>
+      <Card/>
+    </>
+  )
+}
+
+export default App;
