811dd80ccf9f997c4d723f89c89b1f73791f9fc2
ADITYA SINGH <adityasinghk099@gmail.com>
2025-12-10 23:05:12 +0530

    Creating Currency-Convertor App

---
+++ 10-currencyConvertorApp/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export default function useCurrencyInfo(currency) {
+  let [currencyInfo, setCurrencyInfo] = useState({});
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json
+`)
+      .then((res) => res.json())
+      .then((data) => setCurrencyInfo(data[currency]));
+  }, [currency]);
+  console.log(currencyInfo);
+  return currencyInfo;
+};
