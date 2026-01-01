55260fa92378b38aea2e01a4ee1333f3b429135f
ADITYA SINGH <adityasinghk099@gmail.com>
2026-01-01 22:10:41 +0530

    learned createBrowserRouter() method

--- 11-react-router/src/components/Home/Home.jsx
+++ 11-react-router/src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <div className="mx-auto w-full max-w-7xl">
@@ -29,13 +30,10 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?semt=ais_hybrid&w=740&q=80" alt="image1" />
                 </div>
             </aside>
 
-            <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-            </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
--- 11-react-router/src/main.jsx
+++ 11-react-router/src/main.jsx
@@ -1,7 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {RouterProvider } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+import Contact from './components/Contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'about',
+        element: <About/>
+      },
+      {
+        path: 'contact-us',
+        element: <Contact/>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
